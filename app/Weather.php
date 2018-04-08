a94f667939f1ede1d590bddeb8a43fb1bd53608d
Geo4you <kontakt@geo4you.waw.pl>
2018-04-08 20:36:06 +0200

    Dodanie pliku modelu Weather

---
+++ app/Weather.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Weather extends Model
+{
+    
+    // protected $weatherData =
+    // [
+    //     'temperature',
+    //     'wind',
+    //     'humidity',
+    //     'pressure'
+    // ];
+
+}
