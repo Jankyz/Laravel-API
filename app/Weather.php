47633765b17d9a028aa1446bfb36d5996cd49b14
Geo4you <kontakt@geo4you.waw.pl>
2018-04-09 20:27:37 +0200

    zmiana w modelu weather

--- app/Weather.php
+++ app/Weather.php
@@ -7,12 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Weather extends Model
 {
     
-    // protected $weatherData =
-    // [
-    //     'temperature',
-    //     'wind',
-    //     'humidity',
-    //     'pressure'
-    // ];
-
 }
