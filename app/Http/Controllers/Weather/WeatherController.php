cdc88273eac9b7d15a39024ba2299dbcfff4870e
Geo4you <kontakt@geo4you.waw.pl>
2018-04-09 20:27:58 +0200

    znaczne zmiany w controlerze weather

--- app/Http/Controllers/Weather/WeatherController.php
+++ app/Http/Controllers/Weather/WeatherController.php
@@ -16,21 +16,29 @@ class WeatherController extends Controller
     public function weather()
     {
         $weather = Weather::latest()->first();
-        $now = (new DateTime())->format('Y-m-d G:i:s');
-
-        $fromTime = strtotime($weather->created_at);
-        $toTime = strtotime($now);
-
-        $min = round(abs($toTime - $fromTime) / 60, 2);
-
         
-        echo $min;
-
-        if ($min >= 60){
+        if ( !$weather ) {
             $this->storeWeather();
+        } else {
+            $now = (new DateTime())->format('Y-m-d G:i:s');
+    
+            $fromTime = strtotime($weather->created_at);
+            $toTime = strtotime($now);
+    
+            $min = round(abs($toTime - $fromTime) / 60, 2);
+            
+            if ($min >= 60) {
+                $this->storeWeather();
+            }
         }
+        
         $weathers = Weather::latest()->first();
-        return view('weather')->with('weathers', $weathers);
+
+        $icon = $this->getIconWeather($weathers->icon);
+
+        return view('weather')
+            ->with('weathers', $weathers)
+            ->with('icon', $icon);
 
     }
     
@@ -62,9 +70,58 @@ class WeatherController extends Controller
         $weather = new Weather;
         $weather->temperature = $data['main']['temp'];
         $weather->wind = $data['wind']['speed'];
+        $weather->wind_dir = $data['wind']['deg'];
         $weather->humidity = $data['main']['humidity'];
         $weather->pressure = $data['main']['pressure'];
+        $weather->icon = $data['weather']['0']['id'];
         $weather->save();    
+    }
+
+    public function getIconWeather($icon)
+    {
+        switch ($icon) {
+            case 200 : return 'lightning'; break;
+            case 201 : return 'lightning'; break;
+            case 202 : return 'lightning'; break;
+            case 210 : return 'lightning'; break;
+            case 211 : return 'lightning'; break;
+            case 212 : return 'lightning'; break;
+            case 221 : return 'lightning'; break;
+            case 230 : return 'lightning'; break;
+            case 231 : return 'lightning'; break;
+            case 232 : return 'lightning'; break;
+            case 300 : return 'showers'; break;
+            case 301 : return 'showers'; break;
+            case 302 : return 'showers'; break;
+            case 310 : return 'showers'; break;
+            case 311 : return 'showers'; break;
+            case 312 : return 'showers'; break;
+            case 321 : return 'showers'; break;
+            case 500 : return 'rain'; break;
+            case 501 : return 'rain'; break;
+            case 502 : return 'rain'; break;
+            case 503 : return 'rain'; break;
+            case 504 : return 'rain'; break;
+            case 511 : return 'sleet'; break;
+            case 520 : return 'rain'; break;
+            case 521 : return 'rain'; break;
+            case 522 : return 'rain'; break;
+            case 600 : return 'snow'; break;
+            case 601 : return 'snow'; break;
+            case 602 : return 'snow'; break;
+            case 611 : return 'snow'; break;
+            case 621 : return 'snow'; break;
+            case 622 : return 'snow'; break;
+            case 701 : return 'fog'; break;
+            case 711 : return 'fog'; break;
+            case 721 : return 'haze'; break;
+            case 741 : return 'fog'; break;
+            case 800 : return 'sunny'; break;
+            case 801 : return 'cloudy'; break;
+            case 802 : return 'cloudy'; break;
+            case 803 : return 'cloudy'; break;
+            case 804 : return 'sunny-overcast'; break;
+        }
     }
 
 }
